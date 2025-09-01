a77e242688425e895a13da57d86be3891b25516a
trailrunningshelter-web <trailrunningshelter@gmail.com>
2025-09-01 19:00:33 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
 
 const CACHE_NAME = 'allin-timer-v3';
 const ASSETS = [
+const CACHE_NAME = 'allin-timer-v4'; // aumenta la versione per forzare l’update
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 self.addEventListener('install', (evt) => {
   evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
